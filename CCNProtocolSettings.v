38fdb79d511c356bd6608947b0992bd70537828d
Sosuke MORIGUCHI <chiguri.s@gmail.com>
2015-12-21 01:34:25 +0900

    Rename parameter.

--- CCNProtocolSettings.v
+++ CCNProtocolSettings.v
@@ -1,8 +1,8 @@
 (* Written by Sosuke Moriguchi (chiguri), Kwansei Gakuin University *)
 
-(** * Module Type for Resource Control in each Node *)
+(** * Module Type for Resource Management in each Node *)
 
-(* Consistency may be a little difficult to prove *)
+(* Consistency may be a little bit difficult to prove *)
 Require Import List.
 Import ListNotations.
 
@@ -22,20 +22,20 @@ Import OldProtocol.
 Definition Packet := OldProtocol.Packet.
 Definition Event := OldProtocol.Event.
 
-(** Content_get from event list with resource control *)
-Parameter RCContent_get : Node -> forall c : Content_Name, list Event -> option (Content c).
+(** RMF : Resource Management Function, function from event list to content *)
+Parameter RMF : Node -> forall c : Content_Name, list Event -> option (Content c).
 
 (** InitCS should keep its own resources. *)
-Parameter resource_control_keep_InitCS :
-  forall (n : Node) (c : Content_Name), InitCS n c -> forall es : list Event, exists C : Content c, RCContent_get n c es = Some C.
+Parameter RMF_keep_InitCS :
+  forall (n : Node) (c : Content_Name), InitCS n c -> forall es : list Event, exists C : Content c, RMF n c es = Some C.
 
 (** Nodes other than InitCS does not have contents initially. *)
-Parameter resource_control_not_create_content :
-  forall (n : Node) (c : Content_Name), ~InitCS n c -> RCContent_get n c [] = None.
+Parameter RMF_not_create_content :
+  forall (n : Node) (c : Content_Name), ~InitCS n c -> RMF n c [] = None.
 
 (** If once the node seems not to have requested contents, it will not have until it will receive and store the contents. *)
 Parameter resource_control_consistency :
-  forall (n : Node) (c : Content_Name) (es es' : list Event), RCContent_get n c es = None -> (forall C : Content c, ~In (StoreData n c C) es') -> RCContent_get n c (es' ++ es) = None.
+  forall (n : Node) (c : Content_Name) (es es' : list Event), RMF n c es = None -> (forall C : Content c, ~In (StoreData n c C) es') -> RMF n c (es' ++ es) = None.
 
 End CCN_Protocol_Settings.
 
