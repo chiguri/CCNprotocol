69a2259bede5160c16305444bcf8a5ff4f08b07d
Sosuke MORIGUCHI <chiguri.s@gmail.com>
2015-12-25 00:29:54 +0900

    Refine the behavior (Requested state cannot be described by "In" predicate)

--- CCNProtocolWithCM.v
+++ CCNProtocolWithCM.v
@@ -16,6 +16,32 @@ Import Topology.
 
 
 
+(** Exists Request or not *)
+Fixpoint In_Request (v : Node) (c : Content_Name) (es : list Event) : bool :=
+match es with
+| nil => false
+| Request v' c' :: es' =>
+    match Node_eq_dec v v' with
+    | left _ => match Content_Name_eq_dec c c' with
+                | left _ => true
+                | right _ => In_Request v c es'
+                end
+    | right _ => In_Request v c es'
+    end
+| StoreData v' c' _ :: es' =>
+    match Node_eq_dec v v' with
+    | left _ => match Content_Name_eq_dec c c' with
+                | left _ => false
+                | right _ => In_Request v c es'
+                end
+    | right _ => In_Request v c es'
+    end
+| _ :: es' => In_Request v c es'
+end.
+
+
+
+
 (** Definition of behaviors of the CCN protocol *)
 Inductive CCNprotocol : list Event -> list Packet -> Prop :=
 | ccn_init : CCNprotocol nil nil
@@ -59,21 +85,21 @@ Inductive CCNprotocol : list Event -> list Packet -> Prop :=
 | ccn_store_data : forall (v v' : Node) (c : Content_Name) (C : Content c) (es : list Event) (ps1 ps2 ps' : list Packet),
    CCNprotocol es (ps1 ++ Data v v' c C :: ps2) ->
     CMF v' c es = None ->
-    In (Request v' c) es ->
+    In_Request v' c es = true ->
     PIT_list v' c es = nil ->
     ps' = ps1 ++ ps2 ->
     CCNprotocol (StoreData v' c C :: es) ps'
 | ccn_forward_data : forall (v v' : Node) (c : Content_Name) (C : Content c) (es : list Event) (ps1 ps2 ps' : list Packet),
    CCNprotocol es (ps1 ++ Data v v' c C :: ps2) ->
     CMF v' c es = None ->
-    ~ In (Request v' c) es ->
+    In_Request v' c es = false ->
     PIT_list v' c es <> nil ->
     ps' = (PIT_Data v' c C es) ++ ps1 ++ ps2 ->
     CCNprotocol (StoreData v' c C :: ForwardData v' c :: es) ps'
 | ccn_store_forward : forall (v v' : Node) (c : Content_Name) (C : Content c) (es : list Event) (ps1 ps2 ps' : list Packet),
    CCNprotocol es (ps1 ++ Data v v' c C :: ps2) ->
     CMF v' c es = None ->
-    In (Request v' c) es ->
+    In_Request v' c es = true ->
     PIT_list v' c es <> nil ->
     ps' = (PIT_Data v' c C es) ++ ps1 ++ ps2 ->
     CCNprotocol (StoreData v' c C :: ForwardData v' c :: es) ps'
